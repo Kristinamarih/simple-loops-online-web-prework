ac9680b5e79c69e8d4e7540204c52e68e8f3f54a
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-02-20 23:57:39 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -26,7 +26,7 @@ def times_iterator(number_of_times)
   number_of_times.times do
     puts "Welcome to Flatiron School's Web Development Course!"
   end
-end
+
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
 end
