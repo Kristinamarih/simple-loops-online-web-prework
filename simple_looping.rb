6d6edf3ee0e166c070576e25278cfecc24e1d9db
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-02-20 23:54:46 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -3,7 +3,7 @@
 def loop_iterator(number_of_times)
   loop_iterator = 0 
   loop do
-    loop_iterator = +
+    loop_iterator += 1 
     puts "Welcome to Flatiron School's Web Development Course!"
     if loop_iterator >= number_of_times
       break
